9ac53d0e06d38d3ce7f3347595d465c9e157a1fb
Nishat914 <jahannishat914@gmail.com>
2026-07-23 12:16:58 +0600

    implement loading page

---
+++ src/app/(main)/loading.js
@@ -0,0 +1,7 @@
+export default async function Loading() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <span className="loading loading-infinity loading-xl text-[#C86B43]"></span>
+    </div>
+  );
+}
